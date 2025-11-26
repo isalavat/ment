613c4e733ca7fe8aeb7a46b2fb16f8c762835554
Salavat Islamov <49363502+isalavat@users.noreply.github.com>
2025-11-26 21:27:37 +0100

    add some auth stuff

---
+++ frontend-app/src/services/api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const API_BASE_URL = 'http://localhost:8080';
+const ACCESS_TOKEN = 'accessToken';
+const REFREH_TOKEN = 'refreshToken';
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
+    }
+);
+
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const resfreshToken = localStorage.getItem('refreshToken');
+                const response = await axios.post(`${API_BASE_URL}/auth/refresh`, {
+                    resfreshToken
+                });
+                const { accessToken } = response.data;
+                localStorage.setItem(ACCESS_TOKEN, accessToken);
+                originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                return api(originalRequest);
+            } catch (refreshError) {
+                localStorage.removeItem(ACCESS_TOKEN);
+                localStorage.removeItem(REFREH_TOKEN);
+                window.location.href = '/login';
+                return Promise.reject(refreshError)
+            }
+        }
+        return Promise.reject(error)
+    }
+)
---
+++ frontend-app/src/types/auth.ts
@@ -0,0 +1,27 @@
+export type UserRole = "MENTOR" | "MENTEE" | "ADMIN";
+
+export interface User {
+  id: string;
+  email: string;
+  role: UserRole;
+  firstName: string;
+  lastName: string;
+}
+
+export interface AuthResponse {
+    accessToken: string;
+    refreshToken: string;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface RegisterRequest {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    role: UserRole;
+}
