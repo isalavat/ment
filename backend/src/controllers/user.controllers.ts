0a8e65b2e00cec77014f3484ce82fb281eb10d47
Kalys Nurmanbetov <knu@Kalyss-MBP.fritz.box>
2026-01-04 23:27:01 +0100

    introduce signUpService to orchestrate the sign up scenario

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from "express";
 import { CreateUserDTO } from "../schemas/auth.schemas";
-import { createUserUseCase } from "../use-cases/createUserUseCase";
-import { generateTokenUseCase } from "../use-cases/generateTokenUseCase";
+import { signUpService } from "../services/signUpService";
 
 export const createUserController = async (req: Request<{}, {}, CreateUserDTO>, res: Response) => {
-  const user = await createUserUseCase(req.body);
-  const { accessToken, refreshToken } = await generateTokenUseCase(user);
+  const { accessToken, refreshToken, user } = await signUpService.execute(req.body);
 
   return res.status(201).json({
     accessToken,
