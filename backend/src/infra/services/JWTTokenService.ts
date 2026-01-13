b2ea12725a68cbb01d89ca50a18ad17522a49a1e
Kalys Nurmanbetov <knu@Kalyss-MacBook-Pro.local>
2026-01-13 21:37:28 +0100

    Fix token service interface

--- backend/src/infra/services/JWTTokenService.ts
+++ backend/src/infra/services/JWTTokenService.ts
@@ -1,13 +1,15 @@
+import type { Email } from "../../domain/user/value-objects/Email";
+import type { UserId } from "../../domain/user/value-objects/UserId";
 import { signAccessToken, signRefreshToken } from "../../lib/jwt";
-import type { TokenService, Tokens, UserInfoForToken } from "../../services/TokenService";
+import type { TokenService, Tokens } from "../../services/TokenService";
 import { PrismaClientGetway } from "../PrismaClientGetway";
 
 export class JWTTokenService implements TokenService {
-	async generate(info: UserInfoForToken): Promise<Tokens> {
-		const accessToken = signAccessToken({ sub: info.id, email: info.email });
-		const refreshToken = signRefreshToken({ sub: info.id, email: info.email });
+	async generate(userId: UserId, email: Email): Promise<Tokens> {
+		const accessToken = signAccessToken({ sub: userId.value, email: email.value });
+		const refreshToken = signRefreshToken({ sub: userId.value, email: email.value });
 
-		await PrismaClientGetway().refreshToken.create({ data: { token: refreshToken, userId: info.id } });
+		await PrismaClientGetway().refreshToken.create({ data: { token: refreshToken, userId: userId.value } });
 
 		return { accessToken, refreshToken };
 	}
--- backend/src/services/TokenService.ts
+++ backend/src/services/TokenService.ts
@@ -1,7 +1,7 @@
 import type { Email } from "../domain/user/value-objects/Email";
 import type { UserId } from "../domain/user/value-objects/UserId";
 
-type Tokens = { accessToken: string; refreshToken: string };
+export type Tokens = { accessToken: string; refreshToken: string };
 
 export interface TokenService {
 	generate(userId: UserId, email: Email): Promise<Tokens>;
