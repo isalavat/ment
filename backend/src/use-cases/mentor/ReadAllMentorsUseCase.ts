1b55316a42836ad05284e909c2e8d1ceff85a577
Salavat <salavat.islamov.96@gmail.com>
2026-01-20 19:48:14 +0100

    Remove console logs from ReadAllMentorsUseCase

--- backend/src/use-cases/mentor/ReadAllMentorsUseCase.ts
+++ backend/src/use-cases/mentor/ReadAllMentorsUseCase.ts
@@ -5,10 +5,8 @@ export class ReadAllMentorsUseCase {
   constructor(private mentorProfileRepo: MentorProfileRepository) {}
 
   async execute(): Promise<MentorProfile[]> {
-    console.log("ReadAllMentorsUseCase.execute called");
     const mentorProfiles: MentorProfile[] =
       await this.mentorProfileRepo.findAllMentorProfiles();
-    console.log("Retrieved mentor profiles:", mentorProfiles);
     return mentorProfiles;
   }
 }
